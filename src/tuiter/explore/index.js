be1598c2ca70960e10df321977c364812a8ba9e1
ZadenRB <zaden.ruggieroboune@gmail.com>
2023-03-05 17:12:02 -0500

    A6

---
+++ src/tuiter/explore/index.js
@@ -0,0 +1,37 @@
+import PostSummaryList from "../post-summary-list";
+
+const ExploreComponent = () => {
+    return (
+        <>
+            <div className={"d-flex align-items-center mb-2"}>
+                <span className={"input-group me-4"}>
+                    <label className={"input-group-text rounded-end rounded-pill bg-white border-start border-top border-bottom"} for={"search"}>
+                        <i class="bi bi-search"></i>
+                    </label>
+                    <input id={"search"} className={"form-control border-0 border-top border-bottom border-end rounded-start rounded-pill"} type={"text"} placeholder={"Search Tuiter"} />
+                </span>
+                <i className={"fas fa-cog fa-2x text-primary"}></i>
+            </div>
+            <ul class="nav nav-tabs mb-2">
+                <li class="nav-item">
+                    <a class="nav-link active" href="for-you.html">For you</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="trending.html">Trending</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="news.html">News</a>
+                </li>
+            </ul>
+            <div className={"position-relative"}>
+                <img className={"d-block w-100"} src={`/images/starship.jpg`} />
+                <span className={"ms-3 fs-1 fw-bold text-light position-absolute bottom-0 left-0"}>
+                    SpaceX's Starship
+                </span>
+            </div>
+            <PostSummaryList />
+        </>
+    );
+};
+
+export default ExploreComponent;
---
+++ src/tuiter/index.js
@@ -0,0 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import React from "react";
+import ExploreComponent from "./explore";
+import NavigationSidebar from "./navigation-sidebar";
+import WhoToFollowList from "./who-to-follow-list";
+function Tuiter() {
+    return (
+        <div class="row mt-2">
+            <div class="col-2 col-md-2 col-lg-1 col-xl-2">
+                <NavigationSidebar active="explore" />
+            </div>
+            <div class="col-10 col-lg-7 col-xl-6">
+                <ExploreComponent />
+            </div>
+            <div class="d-none d-lg-block col-lg-4 col-xl-4">
+                <WhoToFollowList />
+            </div>
+        </div>
+    );
+}
+export default Tuiter;
